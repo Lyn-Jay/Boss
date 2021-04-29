d6400d18c3e8f9e4f59af4e95e2042df1ba5f5b6
“cms-web <wang.lin@chinaott.net>
2021-04-29 16:26:28 +0800

    启用htto内置工具,添加测试接口

--- src/main/java/wanglin/Boss/controller/TestController.java
+++ src/main/java/wanglin/Boss/controller/TestController.java
@@ -2,14 +2,18 @@ package wanglin.Boss.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController {
+public class TestController{
 
     @GetMapping("/hello")
     public String hello(){
-        return "";
+        return "hello";
+    }
+
+    @PostMapping("/hello/post")
+    public <name> String helloPost(String name){
+       return  "hello," + name;
     }
 }
