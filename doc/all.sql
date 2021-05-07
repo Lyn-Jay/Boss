d80ae3df392e479ba785954c5b583c7cd6778d3f
“cms-web <wang.lin@chinaott.net>
2021-05-07 18:18:09 +0800

    集成mybatis generator,生成示例表代码

--- doc/all.sql
+++ doc/all.sql
@@ -6,4 +6,13 @@
 #      primary key (`id`)
 # )engine=innodb default charset=utf8mb4 comment='测试'
 
-select * from test
+select * from test;
+
+drop table if exists `demo`;
+create table `demo`(
+    `id` bigint not null comment 'id',
+    `name` varchar(50) comment '名称',
+    primary key (`id`)
+)engine=innodb default charset=utf8mb4 comment='测试';
+insert into `demo`(id, name) values (1,'测试');
+
