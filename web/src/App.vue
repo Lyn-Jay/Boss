188c9bae435c5f4c9b005824da86e4b6b8cfb3e2
“cms-web <wang.lin@chinaott.net>
2021-05-17 15:10:54 +0800

    搭建前端页面,导入axios

---
+++ web/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <a-layout id="components-layout-demo-top-side-2">
+    <TheHeader></TheHeader>
+    <router-view></router-view>
+    <TheFooter></TheFooter>
+  </a-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import TheHeader from "@/components/TheHeader.vue";
+import TheFooter from "@/components/TheFooter.vue";
+
+export default defineComponent({
+  components : {
+    TheHeader,
+    TheFooter
+  }
+});
+</script>
+
+<style>
+#components-layout-demo-top-side-2 .logo {
+  float: left;
+  width: 120px;
+  height: 31px;
+  margin: 16px 24px 16px 0;
+  background: rgba(255, 255, 255, 0.3);
+}
+
+.ant-row-rtl #components-layout-demo-top-side-2 .logo {
+  float: right;
+  margin: 16px 0 16px 24px;
+}
+
+.site-layout-background {
+  background: #fff;
+}
+</style>
---
+++ web/src/components/TheFooter.vue
@@ -0,0 +1,13 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    1112
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TheFooter',
+});
+</script>
